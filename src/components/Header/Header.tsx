0ae43e148ee36312c4197327b1f5641734475536
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-09-07 00:19:39 +0300

    ADD: connection wallet

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,37 @@
 import { Container } from "../Container/Container";
 import Icon from "../Icon/Icon";
 
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
 import { Button, HeaderContainer, HeaderStyled } from "./Header.styled";
 
 export const Header = () => {
+	const { address, isConnected } = useAccount();
+	const { connect } = useConnect({
+		connector: new InjectedConnector(),
+	});
+	const { disconnect } = useDisconnect();
+
+	const connectWalletHandler = () => {
+		console.log("connectWalletHandler");
+		connect();
+	};
+
+	if (isConnected)
+		return (
+			<div>
+				Connected to {address}
+				<button onClick={() => disconnect()}>Disconnect</button>
+			</div>
+		);
+
 	return (
 		<HeaderStyled>
 			<Container>
 				<HeaderContainer>
 					<Icon name="logo" width={35} height={20} />
-					<Button>Connect wallet</Button>
+					<Button onClick={connectWalletHandler}> Connect wallet</Button>
 				</HeaderContainer>
 			</Container>
 		</HeaderStyled>
