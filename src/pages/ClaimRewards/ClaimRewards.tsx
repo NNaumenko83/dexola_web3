14e00c565b0885611b3951388815db8a1a83b453
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-09-10 00:48:15 +0300

    FIX: pages components

--- src/pages/ClaimRewards/ClaimRewards.tsx
+++ src/pages/ClaimRewards/ClaimRewards.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ClaimRewards = () => {
 	return <div>ClaimRewards</div>;
 };
--- src/pages/Withdraw/Withdraw.tsx
+++ src/pages/Withdraw/Withdraw.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Withdraw = () => {
 	return <div>Withdraw</div>;
 };
