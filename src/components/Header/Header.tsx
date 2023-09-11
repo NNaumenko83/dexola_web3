4f9a505d491a4755bc38326b0afd5b173d7a9fd4
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-09-12 00:04:25 +0300

    UPD: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import Web3 from "web3";
+// import { useEffect, useState } from "react";
+// import Web3 from "web3";
 
 import { useAccount, useDisconnect } from "wagmi";
 
@@ -8,59 +8,63 @@ import Icon from "../Icon/Icon";
 import { HeaderContainer, HeaderStyled } from "./Header.styled";
 
 // import contractStakingABI from "../../contracts/contract-staking-abi.json";
-import contractTockenTrackingABI from "../../contracts/contract-tokenTracker-abi.json";
+// import contractTockenTrackingABI from "../../contracts/contract-tokenTracker-abi.json";
 import { ConnectToWalletButton } from "../ConnectToWalletButton/ConnectToWalletButton";
+import useWeb3 from "../../hooks/useWeb3";
 
 // Адреси контрактів
 // Адреса контракту на який ми депозитимо токени StarRunner
 // const contractStakingAddress = "0x2f112ed8a96327747565f4d4b4615be8fb89459d";
 // Адреса контракту з токенами StarRunner
-const contractTockenTrackingAddress = "0x59Ec26901B19fDE7a96f6f7f328f12d8f682CB83";
+// const contractTockenTrackingAddress = "0x59Ec26901B19fDE7a96f6f7f328f12d8f682CB83";
 
 export const Header = () => {
 	// Використовуємо хук для отримання аккаунта і стану підключення
 	const { address, isConnected } = useAccount();
-	const [ethBalance, setEthbalance] = useState<bigint | null>(null);
-	const [struBalance, setStruBalance] = useState<bigint | null>(null);
+	console.log("isConnected:", isConnected);
+	const web3 = useWeb3();
+	console.log("web3:", web3);
+	// const [ethBalance, setEthbalance] = useState<bigint | null>(null);
+	// const [struBalance, setStruBalance] = useState<bigint | null>(null);
 	// const [stakedBalance, setStakedBalance] = useState<bigint | null>(null);
 
 	// Ініціалізація Web3 та вказання адреси Infura для взаємодії з блокчейном Ethereum
 	// Infura для отримання інформації з блокчейну та відправлення транзакцій.
 	//  Infura - це популярний сервіс, який надає доступ до Ethereum-нод через HTTP-запити.
-	const web3 = new Web3(
-		new Web3.providers.HttpProvider("https://sepolia.infura.io/v3/35a6a592708b48bc8707f2ba01b3aaf2"),
-	);
+	// const web3 = new Web3(
+	// 	new Web3.providers.HttpProvider("https://sepolia.infura.io/v3/35a6a592708b48bc8707f2ba01b3aaf2"),
+	// );
 
 	// Створюємо об'єкти контрактів на основі їхніх ABI (Application Binary Interface) та адрес в мережі Ethereum.
 	// Можжна викликати методи цих контрактів, які виконують різні дії на блокчейні (наприклад, отримання балансу або здійснення транзакцій).
 	// const contractStaking = new web3.eth.Contract(contractStakingABI, contractStakingAddress) as any;
 
-	const contractTockenTracking = new web3.eth.Contract(contractTockenTrackingABI, contractTockenTrackingAddress) as any;
+	// const contractTockenTracking = new web3.eth.Contract(contractTockenTrackingABI, contractTockenTrackingAddress) as any;
 	// Вибір типу any дозволяє взаємодіяти з об'єктом контракту без строгої типізації TypeScript.
 	// Такий підхід може бути корисний, якщо не відомі точні типи, пов'язані з контрактом.
 
-	useEffect(() => {
-		// Функція для отримання балансу
-		const getBalance = async () => {
-			const balanceEth = await web3.eth.getBalance("0x16a370583Ad9318049700d02f88b752761001a97");
-			console.log("balanceEth:", balanceEth);
-			setEthbalance(balanceEth);
+	// useEffect(() => {
+	// 	// Функція для отримання балансу
+	// 	const getBalance = async () => {
+	// 		const balanceEth = await web3.eth.getBalance("0x16a370583Ad9318049700d02f88b752761001a97");
+	// 		console.log("balanceEth:", balanceEth);
+	// 		setEthbalance(balanceEth);
 
-			// if (address) {
-			// 	const balanceStaked = await contractStaking.methods.balanceOf(address).call();
-			// }
+	// 		// if (address) {
+	// 		// 	const balanceStaked = await contractStaking.methods.balanceOf(address).call();
+	// 		// }
 
-			if (address) {
-				const balanceStruOnWallet = await contractTockenTracking.methods.balanceOf(address).call();
-				console.log("balanceStruOnWallet:", balanceStruOnWallet);
-				setStruBalance(balanceStruOnWallet);
-			}
-		};
+	// 		if (address) {
+	// 			const balanceStruOnWallet = await contractTockenTracking.methods.balanceOf(address).call();
+	// 			console.log("balanceStruOnWallet:", balanceStruOnWallet);
+	// 			setStruBalance(balanceStruOnWallet);
+	// 		}
+	// 	};
 
-		// Виклик функції для отримання балансу при завантаженні компонента
-		getBalance();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	// 	// Виклик функції для отримання балансу при завантаженні компонента
+	// 	getBalance();
+	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
+	// }, []);
 
 	// Використовуємо хук для відключення аккаунта
 	const { disconnect } = useDisconnect();
@@ -75,8 +79,8 @@ export const Header = () => {
 						<div>
 							Connected to {address}
 							<button onClick={() => disconnect()}>Disconnect</button>
-							<div>{struBalance?.toString()} STRU</div>
-							<div>{ethBalance?.toString()} ETH</div>
+							{/* <div>{struBalance?.toString()} STRU</div>
+							<div>{ethBalance?.toString()} ETH</div> */}
 						</div>
 					) : (
 						<ConnectToWalletButton />
--- src/hooks/useWeb3.ts
+++ src/hooks/useWeb3.ts
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
 import Web3 from "web3";
 
-const useWeb3 = (): Web3 | null => {
+const useWeb3 = (): { web3: Web3 | null; error: Error | null } => {
 	const [web3, setWeb3] = useState<Web3 | null>(null);
+	const [error, setError] = useState<Error | null>(null);
+
+	const { address, isConnected } = useAccount();
+	console.log("address:", address);
+	console.log("isConnected:", isConnected);
 
 	useEffect(() => {
-		const infuraUrl = "https://sepolia.infura.io/v3/35a6a592708b48bc8707f2ba01b3aaf2";
-		const web3Instance = new Web3(new Web3.providers.HttpProvider(infuraUrl));
+		try {
+			const infuraUrl = "https://sepolia.infura.io/v3/35a6a592708b48bc8707f2ba01b3aaf2";
+			const web3Instance = new Web3(new Web3.providers.HttpProvider(infuraUrl));
 
-		setWeb3(web3Instance);
+			setWeb3(web3Instance);
+		} catch (err: any) {
+			setError(err);
+		}
 	}, []);
 
-	return web3;
+	return { web3, error };
 };
 
 export default useWeb3;
