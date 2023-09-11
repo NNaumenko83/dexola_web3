2ba3987da26e999289ea5fd107fdaf59182f214e
Nikolay Naumenko <nnv2006@ukr.net>
2023-09-11 15:35:25 +0300

    UPD: table

--- src/components/HeroTable/HeroTable.styled.ts
+++ src/components/HeroTable/HeroTable.styled.ts
@@ -3,6 +3,7 @@ import Icon from "../Icon/Icon";
 
 export const Table = styled.table`
 	width: 100%;
+	max-width: 662px;
 	border-collapse: collapse;
 	/* border-spacing: 4px; */
 `;
@@ -23,7 +24,10 @@ export const TableHead = styled.thead`
 
 export const TableBody = styled.tbody`
 	width: 100%;
+
 	/* border: 1px solid blue; */
+	@media screen and (min-width: 375px) {
+	}
 `;
 
 export const TableRow = styled.tr`
@@ -41,6 +45,28 @@ export const TableRow = styled.tr`
 		& > :nth-child(3) {
 			min-width: 47px;
 		}
+
+		& > :nth-child(4) {
+			max-width: 244px;
+		}
+	}
+
+	@media screen and (min-width: 744px) {
+		& > :nth-child(1) {
+			width: 194px;
+		}
+
+		& > :nth-child(2) {
+			width: 112px;
+		}
+
+		& > :nth-child(3) {
+			width: 112x;
+		}
+
+		& > :nth-child(4) {
+			width: 244px;
+		}
 	}
 `;
 
@@ -54,6 +80,12 @@ export const TableBottomRow = styled(TableRow)`
 	letter-spacing: -0.24px;
 
 	@media screen and (min-width: 744px) {
+		font-size: 16px;
+
+		line-height: normal;
+		letter-spacing: -0.32px;
+	}
+	@media screen and (min-width: 1440px) {
 		display: none;
 	}
 `;
@@ -63,11 +95,22 @@ export const TableCell = styled.div`
 	display: flex;
 	justify-content: flex-start;
 	align-items: flex-end;
-	margin-bottom: 4px;
+	@media screen and (max-width: 743px) {
+		margin-bottom: 4px;
+	}
+
+	@media screen and (min-width: 744px) {
+		align-items: baseline;
+	}
 `;
 
 export const TableCellApy = styled(TableCell)`
 	align-items: center;
+	@media screen and (min-width: 744px) {
+		& > :nth-child(2) {
+			transform: translate(0, 3px);
+		}
+	}
 `;
 
 export const HelpIcon = styled(Icon)`
@@ -81,6 +124,7 @@ export const HelpIcon = styled(Icon)`
 		width: 24px;
 		height: 24px;
 		stroke: #ffffff;
+		margin-left: 4px;
 	}
 `;
 
@@ -92,6 +136,13 @@ export const TextValue = styled.span`
 	font-weight: 700;
 	line-height: normal;
 	letter-spacing: -0.36px;
+
+	@media screen and (min-width: 744px) {
+		font-size: 28px;
+
+		line-height: 48px; /* 171.429% */
+		text-transform: uppercase;
+	}
 
 	/* outline: 1px solid red; */
 `;
@@ -107,10 +158,22 @@ export const UnitName = styled.span`
 	line-height: 20px; /* 166.667% */
 	text-transform: uppercase;
 
+	@media screen and (min-width: 744px) {
+		font-size: 16px;
+
+		font-weight: 400;
+		line-height: 20px; /* 125% */
+	}
+
 	/* outline: 1px solid green; */
 `;
 
 export const UnitIconWrapper = styled.div`
 	display: flex;
 	align-items: center;
+	@media screen and (min-width: 744px) {
+		& > :nth-child(2) {
+			transform: translate(0, -2px);
+		}
+	}
 `;
