da5a5fe1345a55437e5cab6231d4cbb574fa1b1b
Nikolay Naumenko <nnv2006@ukr.net>
2023-09-13 12:26:53 +0300

    UPD: form stake

--- src/components/StakeForm/StakeForm.tsx
+++ src/components/StakeForm/StakeForm.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { toBigInt } from "web3-utils";
+// import { toBigInt } from "web3-utils";
 import { useWeb3 } from "../../hooks/useWeb3";
 import { useAccount } from "wagmi";
+import { usePrepareContractWrite, useContractWrite } from "wagmi";
+
+// import contractStakingABI from "../contracts/contract-staking-abi.json";
+import contractStarRunnerTokenABI from "../../contracts/contract-tokenTracker-abi.json";
 
 export const StakeForm = () => {
 	const { address } = useAccount();
+
+	const { config } = usePrepareContractWrite({
+		address: "0x59Ec26901B19fDE7a96f6f7f328f12d8f682CB83",
+		abi: contractStarRunnerTokenABI,
+		functionName: "approve",
+		args: ["0x2f112ed8a96327747565f4d4b4615be8fb89459d", 100],
+	});
+
+	const { data, write } = useContractWrite(config);
+
+	console.log("data:", data);
+
+	console.log("config:", config);
 	console.log("address:", address);
 	// Треба використати метод approve
 	// approve(address spender, uint256 allowance) - стандарт ERC20, дозвіл на зняття
@@ -16,26 +33,33 @@ export const StakeForm = () => {
 	console.log("contractStaking:", contractStaking);
 	console.log("contractStarRunnerToken:", contractStarRunnerToken);
 
-	const stakeToken = async (numberStake: bigint) => {
-		try {
-			await contractStarRunnerToken.methods
-				.approve("0x2f112ed8a96327747565f4d4b4615be8fb89459d", numberStake)
-				.send({ from: address });
-			console.log("before contractStaking");
-			await contractStaking.methods.stake(numberStake).send({ from: address });
-		} catch (error) {
-			console.error(error);
-		}
-	};
+	// const stakeToken = async () => {
+	// 	try {
+	// 		await contractStarRunnerToken.methods
+	// 			.approve("0x2f112ed8a96327747565f4d4b4615be8fb89459d", 100)
+	// 			.send({ from: address });
+	// 		console.log("before contractStaking");
+	// 		await contractStaking.methods.stake(100).send({ from: address });
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 	}
+	// };
 
 	const onSubmitHandler: React.FormEventHandler<HTMLFormElement> = e => {
 		e.preventDefault();
-		const { stake } = e.target.elements;
+		if (write) {
+			write();
+		}
 
-		const myNumber = toBigInt(stake.value);
-		console.log("myNumber:", myNumber);
+		// const form = e.target as HTMLFormElement;
 
-		stakeToken(myNumber);
+		// const value = form.elements.stake;
+		// console.log("value:", value);
+
+		// const myNumber = toBigInt(stake.value);
+		// console.log("myNumber:", myNumber);
+
+		stakeToken();
 	};
 	return (
 		<form
