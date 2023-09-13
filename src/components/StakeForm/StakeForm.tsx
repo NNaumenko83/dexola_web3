1c8ae8aa094d8255edb53c92da86cc8f12fb370a
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-09-13 21:58:47 +0300

    Test for mobile

--- src/components/StakeForm/StakeForm.tsx
+++ src/components/StakeForm/StakeForm.tsx
@@ -23,7 +23,7 @@ export const StakeForm = () => {
 		address: "0x59Ec26901B19fDE7a96f6f7f328f12d8f682CB83",
 		abi: contractStarRunnerTokenABI,
 		functionName: "approve",
-		args: ["0x2f112ed8a96327747565f4d4b4615be8fb89459d", numberOfSrtu],
+		args: ["0x2f112ed8a96327747565f4d4b4615be8fb89459d", 20000],
 	});
 
 	const { config: stakeConfig } = usePrepareContractWrite({
@@ -31,6 +31,7 @@ export const StakeForm = () => {
 		abi: contractStakingABI,
 		functionName: "stake",
 		args: [numberOfSrtu],
+		enabled: Boolean(numberOfSrtu),
 	});
 
 	const { data: approveData, write: approve } = useContractWrite(approveConfig);
