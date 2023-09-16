d941147997d5b9bf695a7cbb196ef571b0498c92
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-09-16 12:52:57 +0300

    UPD: ClaimRewards and Withdraw

--- src/pages/ClaimRewards/ClaimRewards.tsx
+++ src/pages/ClaimRewards/ClaimRewards.tsx
@@ -1,13 +1,34 @@
 import { useAccount } from "wagmi";
 import { Container } from "../../components/Container/Container";
 import { Form } from "../../components/Form/Form";
+import { PageTitleWrapper } from "../../components/PageTitleWrapper/PageTitleWrapper";
+import { PageTitle } from "../../components/PageTitle/PageTitle";
+import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
 
 import { NotConnectedWrapper } from "../../components/NotConnectedWrapper/NotConnectedWrapper";
 
 const ClaimRewards = () => {
 	const { isConnected } = useAccount();
 
-	return <Container>{!isConnected ? <NotConnectedWrapper /> : <Form />}</Container>;
+	return (
+		<Container>
+			<PageWrapper>
+				{!isConnected ? (
+					<NotConnectedWrapper />
+				) : (
+					<>
+						<PageTitleWrapper>
+							<PageTitle>Claim rewards</PageTitle>
+							<p>
+								Reward rate:<span>1 STRU/week</span>
+							</p>
+						</PageTitleWrapper>
+						<Form />
+					</>
+				)}
+			</PageWrapper>
+		</Container>
+	);
 };
 
 export default ClaimRewards;
--- src/pages/Withdraw/Withdraw.tsx
+++ src/pages/Withdraw/Withdraw.tsx
@@ -2,11 +2,31 @@ import { useAccount } from "wagmi";
 import { Container } from "../../components/Container/Container";
 import { Form } from "../../components/Form/Form";
 import { NotConnectedWrapper } from "../../components/NotConnectedWrapper/NotConnectedWrapper";
+import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
+import { PageTitleWrapper } from "../../components/PageTitleWrapper/PageTitleWrapper";
+import { PageTitle } from "../../components/PageTitle/PageTitle";
 
 const Withdraw = () => {
 	const { isConnected } = useAccount();
-
-	return <Container>{!isConnected ? <NotConnectedWrapper /> : <Form />}</Container>;
+	return (
+		<Container>
+			<PageWrapper>
+				{!isConnected ? (
+					<NotConnectedWrapper />
+				) : (
+					<>
+						<PageTitleWrapper>
+							<PageTitle>Withdraw</PageTitle>
+							<p>
+								Reward rate:<span>1 STRU/week</span>
+							</p>
+						</PageTitleWrapper>
+						<Form />
+					</>
+				)}
+			</PageWrapper>
+		</Container>
+	);
 };
 
 export default Withdraw;
